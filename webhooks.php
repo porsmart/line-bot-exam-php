20709cf0b389102d98e143ab47a6da9d1ab10541
porsmart <39628446+porsmart@users.noreply.github.com>
2018-05-25 22:38:16 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'Ht5ztMNlJ9qwZoXYqy4iwfCJpERAYBYYbBKpbMLwCiItqXahzfzM7wprdfSWKEs9LdGj5IfG3dM96/BSf3DbqIh27Ga9i2X9Ib1qXDeB/g4AFVrV126OA8iRaJYpXN0DVNRAbq3qna9na6NIr1/sHQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
